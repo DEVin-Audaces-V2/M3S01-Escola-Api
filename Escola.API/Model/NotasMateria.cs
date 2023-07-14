aeff5e024b1a7ffc17af45887625bbd18439a17b
Camila Zelenovsky <camila_zele@hotmail.com>
2023-07-13 22:17:12 -0300

    model NotasMateria

--- Escola.API/Model/NotasMateria.cs
+++ Escola.API/Model/NotasMateria.cs
@@ -8,5 +8,12 @@ namespace Escola.API.Model
     public class NotasMateria
     {
         public int Id { get; set; }
+        public Boletim Boletim { get; set; }
+        public int BoletimId { get; set; }
+        public int Materia { get; set; }
+        public int MateriaId { get; set; }
+        public int Nota { get; set; }
+
+
     }
 }
